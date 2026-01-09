ce42340375e41866cae6a792faab54aa705a60a7
shreyau12345 <shreyajaiswal131205@gmail.com>
2026-01-09 14:48:58 +0530

    Add files via upload

---
+++ shreya.py
@@ -0,0 +1,21 @@
+# print("we are going \n bye" )
+# print("""do not go there print("hey everyone")
+# print("okey
+#       jdfjakilfjhfg
+#       mkluifgtrf jkuyfg ngcb 
+#       yeahhhhhh""")
+# print("yesss")
+# print("the answer is", 42, "and pi is approximately", 3.14)
+# x = " divi "
+# y = " nagwanshi "
+
+# print("hello," + x +""+ y )
+# z = 20
+# print(f"I am {z} years old")
+# print("what,", end="")
+# print(" Happened!")
+# print("her name is divi. \n shakshi \n komal \n priya")
+# print(""" she is girl 
+#       she knows 
+#       she did
+#       where are you""")
