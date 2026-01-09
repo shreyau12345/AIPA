ff2cbd120fcc4aa017521e847975bd15ddcc826c
shreyau12345 <shreyajaiswal131205@gmail.com>
2026-01-09 15:13:41 +0530

    Add files via upload

---
+++ mainfunction.py
@@ -0,0 +1,38 @@
+# def main():
+#     try:
+#         print("1. Addition")
+#         print("2. substraction")
+#         print("3. Multiply")
+#         print("4. division")
+#         choice=int(input("Enter your choice: "))
+#         x=int(input("Enter your number "))
+#         y=int(input("Enter your number "))
+#         if choice ==1:
+#             result1=x+y
+#             print(result1)
+#         elif choice ==2: 
+#             result2=x-y
+#             print(result2)
+#         elif choice ==3:
+#             result3=x*y
+#             print(result3)
+#         elif choice ==4:
+#             if y!=0:
+#                 result= x/y
+#                 print(result)
+#             else:
+#                 print("can't be divided ")
+#     except ZeroDivisionError:
+#         print("can't divided by zero")
+#     except ValueError:
+#         print("can't divided by string")
+#     else:
+#         print("numbers are ", x,y)
+#     finally:
+#         print("Done!")
+#     # if y==0:
+#     #     except ZeroDivisionError:
+#     # result =x/y
+    
+# if __name__=="__main__":
+#     main() 
