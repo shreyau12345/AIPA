072ec6d6b79ff7278285a2ec62ce72694e219c6f
shreyau12345 <shreyajaiswal131205@gmail.com>
2026-01-09 14:46:41 +0530

    Add files via upload

---
+++ datastructure.py
@@ -0,0 +1,71 @@
+# x=(1,2,3,4)
+# y=list(x)
+# y.append(7)
+# print(y)
+
+# x=(1,"AIPA",2, "CSA",3,"EM")
+# y=(4,"COSMO",5,"DM","FDT")
+# z=x=y
+# print(z)
+
+                   # FLOWCONTROL
+
+# trade=("AIPA", "COPA", "COSMO", "EM", "CSA")
+# for i in trade: 
+#     if i == "COPA":
+#         pass
+#     if i == "COSMO":
+#         continue 
+#     if i == "EM":
+#         break
+#     print(i)
+
+# trade=("AIPA", "COPA", "COSMO", "EM", "CSA", "FASHION DESIGNING")
+# print(trade) 
+# for i in trade:
+#     if i == "EM":
+#         continue
+#     print(i)
+#     # if i == "CSA":
+#     #     break
+#     # print(i)
+# print("EM trade skipped")
+# # print(i[:4])
+# print("END OF CONDITION") 
+
+# x=[1,2,3,4,5,6]
+# y=[1,2,3,5,4,6]
+# d=y.pop()
+# print(x)
+# print(d)
+
+# x=("apple, banana, graps, pineapple, orange")
+# y=(1,2,3,4,5,6,7)
+
+#datastructurre
+
+# name=["prati", "shivali", "gungun", "riya" "priya", "soniya"]
+# trade=("AIPA", "EM", "cosmo", "copa")
+# subject={"enlish", "hindi", "maths", "Science"}
+# marks={
+#     "prati":[60,30,35,65],
+#     "shivali":[55,68,32,14],
+#     "gungun":[66,45,58,52],
+#     "riya":[65,85,95,45],
+#     "priya":[98,56,21,32],
+#     "soniya":[56,45,85,45],
+#     }
+# print(name)
+# print(trade)
+# print(subject)
+# print(marks)
+
+# print(name)
+# print()
+# print("english" in subject)
+# print("prati" in name)
+# name.insert("1,simmi")
+# name.append("soni")
+# marks["gungun"]=[66,45,58,52]
+# print(marks)
+
